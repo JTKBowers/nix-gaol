1f32a981475c1452c7092759db89cb64a559763f
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-08-30 22:47:17 +1200

    Add option to log the generated bubblewrap command

--- lib.nix
+++ lib.nix
@@ -1,7 +1,7 @@
 rec {
   deps = nixpkgs: pkg: nixpkgs.lib.strings.splitString "\n" (nixpkgs.lib.strings.fileContents (nixpkgs.writeReferencesToFile pkg));
 
-  generateWrapperScript = pkgs: {pkg, name}: pkgs.writeShellScriptBin "bwrapped-${name}" ''
+  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand ? false}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
 ${pkgs.bubblewrap}/bin/bwrap --unshare-all ${builtins.toString (map (x: "--ro-bind ${x} ${x}") (deps pkgs pkg))} ${pkg}/bin/${name} "$@"
 ''; 
 }
