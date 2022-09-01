66256e9a14a5e210aa2a0778994ba01bcb41bad0
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-01 22:43:52 +1200

    Add helper function to ro-bind a list of paths.

--- lib.nix
+++ lib.nix
@@ -1,7 +1,8 @@
 rec {
   deps = nixpkgs: pkg: nixpkgs.lib.strings.splitString "\n" (nixpkgs.lib.strings.fileContents (nixpkgs.writeReferencesToFile pkg));
 
+  generateBindArgs = paths: builtins.toString (map (dir: "--ro-bind ${dir} ${dir}") paths);
   generateWrapperScript = pkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
-${pkgs.bubblewrap}/bin/bwrap --unshare-all ${builtins.toString (map (x: "--ro-bind ${x} ${x}") (deps pkgs pkg))} ${pkg}/bin/${name} "$@"
+${pkgs.bubblewrap}/bin/bwrap --unshare-all ${generateBindArgs (deps pkgs pkg)} ${pkg}/bin/${name} "$@"
 ''; 
 }
