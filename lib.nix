1c93378b717d30a8255aa3c861a32d55397463f5
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-02 22:42:12 +1200

    Add option to ro-bind the current working directory.

--- lib.nix
+++ lib.nix
@@ -1,12 +1,13 @@
 rec {
   deps = nixpkgs: pkg: nixpkgs.lib.strings.splitString "\n" (nixpkgs.lib.strings.fileContents (nixpkgs.writeReferencesToFile pkg));
 
-  generateBindArgs = paths: builtins.toString (map (dir: "--ro-bind ${dir} ${dir}") paths);
-  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, roBindDirs}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
-${pkgs.bubblewrap}/bin/bwrap --unshare-all ${generateBindArgs roBindDirs} ${pkg}/bin/${name} "$@"
+  roBindDirectory = path: "--ro-bind ${path} ${path}";
+  generateBindArgs = paths: builtins.toString (map roBindDirectory paths);
+  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, roBindDirs, roBindCwd}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
+${pkgs.bubblewrap}/bin/bwrap --unshare-all ${generateBindArgs roBindDirs} ${if roBindCwd then roBindDirectory "$(pwd)" else ""} ${pkg}/bin/${name} "$@"
 '';
-  wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false, extraRoBindDirs? []}: let
+  wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false, extraRoBindDirs? [], roBindCwd ? false}: let
     pkgDeps = deps nixpkgs pkg;
     roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs);
-  in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; roBindDirs = roBindDirs;};
+  in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; roBindDirs = roBindDirs; roBindCwd = roBindCwd;};
 }
