4ed427913a14e120a20b73a9c30e8a70defb93ec
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-02 22:27:27 +1200

    Add user-facing function for wrapping a whole package.

--- lib.nix
+++ lib.nix
@@ -2,7 +2,11 @@ rec {
   deps = nixpkgs: pkg: nixpkgs.lib.strings.splitString "\n" (nixpkgs.lib.strings.fileContents (nixpkgs.writeReferencesToFile pkg));
 
   generateBindArgs = paths: builtins.toString (map (dir: "--ro-bind ${dir} ${dir}") paths);
-  generateWrapperScript = pkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
-${pkgs.bubblewrap}/bin/bwrap --unshare-all ${generateBindArgs (deps pkgs pkg)} ${pkg}/bin/${name} "$@"
-''; 
+  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, roBindDirs}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
+${pkgs.bubblewrap}/bin/bwrap --unshare-all ${generateBindArgs roBindDirs} ${pkg}/bin/${name} "$@"
+'';
+  wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false}: let
+    pkgDeps = deps nixpkgs pkg;
+    roBindDirs = pkgDeps;
+  in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; roBindDirs = roBindDirs;};
 }
