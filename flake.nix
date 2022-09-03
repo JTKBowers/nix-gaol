68822d5a5a7ed8d5897894ed5bc46f1743b5a2b1
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-03 21:03:38 +1200

    Format.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,27 @@
 {
   description = "A flake describing functions to wrap packages using bubblewrap";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }: let
-    # These are all the platforms that contain a bubblewrap package
-    # As this flake only provides wrappers for calling bubblewrap, we can only support the platforms that bubblewrap supports
-    supportedPlatforms = builtins.filter (x: (builtins.getAttr x nixpkgs.legacyPackages) ? busybox) (builtins.attrNames nixpkgs.legacyPackages);
-  in
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      # These are all the platforms that contain a bubblewrap package
+      # As this flake only provides wrappers for calling bubblewrap, we can only support the platforms that bubblewrap supports
+      supportedPlatforms = builtins.filter (x: (builtins.getAttr x nixpkgs.legacyPackages) ? busybox) (builtins.attrNames nixpkgs.legacyPackages);
+    in
     flake-utils.lib.eachSystem supportedPlatforms (system: {
-      packages = let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        hello-bwrapped = (import ./lib.nix).wrapPackage pkgs {pkg=pkgs.hello;};
-        helix-bwrapped = (import ./lib.nix).wrapPackage pkgs {pkg=pkgs.helix; name="hx"; bindCwd = true;extraDepPkgs=[pkgs.rust-analyzer pkgs.rnix-lsp]; envs = {HOME="$HOME"; TERM="$TERM"; COLORTERM="$COLORTERM";};};
-        hello-wayland-bwrapped = pkgs.writeShellScriptBin "hello-wayland-bwrapped" ''set -eux
+      packages =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          hello-bwrapped = (import ./lib.nix).wrapPackage pkgs { pkg = pkgs.hello; };
+          helix-bwrapped = (import ./lib.nix).wrapPackage pkgs {
+            pkg = pkgs.helix;
+            name = "hx";
+            bindCwd = true;
+            extraDepPkgs = [ pkgs.rust-analyzer pkgs.rnix-lsp ];
+            envs = { HOME = "$HOME"; TERM = "$TERM"; COLORTERM = "$COLORTERM"; };
+          };
+          hello-wayland-bwrapped = pkgs.writeShellScriptBin "hello-wayland-bwrapped" ''set -eux
 ${pkgs.bubblewrap}/bin/bwrap --unshare-all \
 ${let deps = nixpkgs.lib.strings.splitString "\n" "${nixpkgs.lib.strings.fileContents (pkgs.writeReferencesToFile pkgs.hello-wayland)}"; in builtins.toString (map (x: "--ro-bind ${x} ${x}") deps)} \
 --ro-bind $XDG_RUNTIME_DIR/wayland-0 $XDG_RUNTIME_DIR/wayland-0 \
@@ -30,6 +39,6 @@ ${let deps = nixpkgs.lib.strings.splitString "\n" "${nixpkgs.lib.strings.fileCon
     --setenv LIBGL_DEBUG verbose \
     ${pkgs.hello-wayland}/bin/hello-wayland "$@"
 '';
-      };
+        };
     });
 }
--- lib.nix
+++ lib.nix
@@ -8,13 +8,37 @@ rec {
 
   setEnv = name: value: "--setenv ${name} ${value}";
   generateEnvArgs = pkgs: envs: builtins.toString (pkgs.lib.attrsets.mapAttrsToList setEnv envs);
-  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, bindDirs, roBindDirs, envs}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
+  generateWrapperScript = pkgs: { pkg
+                                , name
+                                , logGeneratedCommand
+                                , bindDirs
+                                , roBindDirs
+                                , envs
+                                }:
+    pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
 ${pkgs.bubblewrap}/bin/bwrap --unshare-all --clearenv ${generateEnvArgs pkgs envs} ${generateBindArgs bindDirs} ${generateRoBindArgs roBindDirs} ${pkg}/bin/${name} "$@"
 '';
-  wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false, extraBindDirs ? [], extraRoBindDirs? [], bindCwd ? false, envs ? {}, extraDepPkgs ? []}: let
-    pkgDeps = (deps nixpkgs pkg) ++ (builtins.concatMap (pkg: deps nixpkgs pkg) extraDepPkgs);
-    bindDirs = extraBindDirs ++ (if bindCwd == true then ["$(pwd)"] else []);
-    roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs ++ (if bindCwd == "ro" then ["$(pwd)"] else []));
-    mergedEnvs = {PATH = "$PATH:${nixpkgs.lib.strings.concatMapStringsSep ":" (dep: "${dep}/bin") extraDepPkgs}"; } // envs;
-  in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; bindDirs = bindDirs; roBindDirs = roBindDirs; envs = mergedEnvs;};
+  wrapPackage = nixpkgs: { pkg
+                         , name ? pkg.pname
+                         , logGeneratedCommand ? false
+                         , extraBindDirs ? [ ]
+                         , extraRoBindDirs ? [ ]
+                         , bindCwd ? false
+                         , envs ? { }
+                         , extraDepPkgs ? [ ]
+                         }:
+    let
+      pkgDeps = (deps nixpkgs pkg) ++ (builtins.concatMap (pkg: deps nixpkgs pkg) extraDepPkgs);
+      bindDirs = extraBindDirs ++ (if bindCwd == true then [ "$(pwd)" ] else [ ]);
+      roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs ++ (if bindCwd == "ro" then [ "$(pwd)" ] else [ ]));
+      mergedEnvs = { PATH = "$PATH:${nixpkgs.lib.strings.concatMapStringsSep ":" (dep: "${dep}/bin") extraDepPkgs}"; } // envs;
+    in
+    generateWrapperScript nixpkgs {
+      pkg = pkg;
+      name = name;
+      logGeneratedCommand = logGeneratedCommand;
+      bindDirs = bindDirs;
+      roBindDirs = roBindDirs;
+      envs = mergedEnvs;
+    };
 }
