c7459f95f0fd6208af05636c0071bce6a283dd06
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-03 20:51:35 +1200

    Tidy up handling of ro-binding cwd.

--- lib.nix
+++ lib.nix
@@ -6,12 +6,12 @@ rec {
 
   setEnv = name: value: "--setenv ${name} ${value}";
   generateEnvArgs = pkgs: envs: builtins.toString (pkgs.lib.attrsets.mapAttrsToList setEnv envs);
-  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, roBindDirs, roBindCwd, envs}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
-${pkgs.bubblewrap}/bin/bwrap --unshare-all --clearenv ${generateEnvArgs pkgs envs} ${generateBindArgs roBindDirs} ${if roBindCwd then roBindDirectory "$(pwd)" else ""} ${pkg}/bin/${name} "$@"
+  generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, roBindDirs, envs}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
+${pkgs.bubblewrap}/bin/bwrap --unshare-all --clearenv ${generateEnvArgs pkgs envs} ${generateBindArgs roBindDirs} ${pkg}/bin/${name} "$@"
 '';
   wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false, extraRoBindDirs? [], roBindCwd ? false, envs ? {}, extraDepPkgs ? []}: let
     pkgDeps = (deps nixpkgs pkg) ++ (builtins.concatMap (pkg: deps nixpkgs pkg) extraDepPkgs);
-    roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs);
+    roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs ++ (if roBindCwd then ["$(pwd)"] else []));
     mergedEnvs = {PATH = "$PATH:${nixpkgs.lib.strings.concatMapStringsSep ":" (dep: "${dep}/bin") extraDepPkgs}"; } // envs;
-  in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; roBindDirs = roBindDirs; roBindCwd = roBindCwd; envs = mergedEnvs;};
+  in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; roBindDirs = roBindDirs; envs = mergedEnvs;};
 }
