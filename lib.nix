9bda9de947746216c1cf252b3077d3278f6e36d8
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-03 20:58:53 +1200

    change bindCwd option to allow for read-write,read-only or no binding of cwd.

--- lib.nix
+++ lib.nix
@@ -11,9 +11,10 @@ rec {
   generateWrapperScript = pkgs: {pkg, name, logGeneratedCommand, bindDirs, roBindDirs, envs}: pkgs.writeShellScriptBin "bwrapped-${name}" ''set -e${if logGeneratedCommand then "x" else ""}
 ${pkgs.bubblewrap}/bin/bwrap --unshare-all --clearenv ${generateEnvArgs pkgs envs} ${generateBindArgs bindDirs} ${generateRoBindArgs roBindDirs} ${pkg}/bin/${name} "$@"
 '';
-  wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false, bindDirs ? [], extraRoBindDirs? [], roBindCwd ? false, envs ? {}, extraDepPkgs ? []}: let
+  wrapPackage = nixpkgs: {pkg, name ? pkg.pname, logGeneratedCommand ? false, extraBindDirs ? [], extraRoBindDirs? [], bindCwd ? false, envs ? {}, extraDepPkgs ? []}: let
     pkgDeps = (deps nixpkgs pkg) ++ (builtins.concatMap (pkg: deps nixpkgs pkg) extraDepPkgs);
-    roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs ++ (if roBindCwd then ["$(pwd)"] else []));
+    bindDirs = extraBindDirs ++ (if bindCwd == true then ["$(pwd)"] else []);
+    roBindDirs = nixpkgs.lib.lists.unique (pkgDeps ++ extraRoBindDirs ++ (if bindCwd == "ro" then ["$(pwd)"] else []));
     mergedEnvs = {PATH = "$PATH:${nixpkgs.lib.strings.concatMapStringsSep ":" (dep: "${dep}/bin") extraDepPkgs}"; } // envs;
   in generateWrapperScript nixpkgs {pkg = pkg; name = name; logGeneratedCommand = logGeneratedCommand; bindDirs = bindDirs; roBindDirs = roBindDirs; envs = mergedEnvs;};
 }
