a4f1463335a886b96dc87a4379f13710fc428321
Jamie Bowers <james.t.k.bowers@gmail.com>
2022-09-04 18:27:58 +1200

    Instrument the wrapped program instead of bwrap itself when strace is enabled 🤦

--- lib.nix
+++ lib.nix
@@ -18,7 +18,7 @@ rec {
                                 , extraArgs
                                 }:
     pkgs.writeShellScriptBin name ''set -e${if logGeneratedCommand then "x" else ""}
-${if strace then "${pkgs.strace}/bin/strace " else ""}${pkgs.bubblewrap}/bin/bwrap --unshare-all --clearenv ${generateEnvArgs pkgs envs} ${generateBindArgs bindDirs} ${generateRoBindArgs roBindDirs} ${builtins.toString extraArgs} ${pkg}/bin/${name} "$@"
+${pkgs.bubblewrap}/bin/bwrap --unshare-all --clearenv ${generateEnvArgs pkgs envs} ${generateBindArgs bindDirs} ${generateRoBindArgs roBindDirs} ${builtins.toString extraArgs} ${if strace then "${pkgs.strace}/bin/strace " else ""}${pkg}/bin/${name} "$@"
 '';
   wrapPackage = nixpkgs: { pkg
                          , name ? pkg.pname
